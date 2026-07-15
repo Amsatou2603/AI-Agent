8eeb224f7fbb692d1874e6bde6515d0397a15ce7
Amsatou Ndiaye <ndiayeamsatou26@gmail.com>
2026-07-15 12:42:25 +0000

    chore: ajouter pyrefly ignore sur imports dans urls.py

--- senegal_agent_ia/urls.py
+++ senegal_agent_ia/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+# pyrefly: ignore [missing-import]
 from django.contrib import admin
+# pyrefly: ignore [missing-import]
 from django.urls import path
 
 from statistiques.views import accueil
